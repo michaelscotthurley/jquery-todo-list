4bdabb9aecc036a39fecfd90830399f7f77ccd58
michaelscotthurley <michaelscotthurley>
2016-01-26 22:36:55 -0800

    Completed To-Do list in jQuery

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+$('#newtask').on('submit', function(e) {
+	e.preventDefault();
+	var $newThing = $('#newtask input').val();
+	if ($newThing != '') {
+		$('#newtask input').val('');
+		$('ul').append('<li>' + $newThing + '<img src="../images/remove.png" />' + '</li>')
+	}
+});
+
+$('ul').on('click', 'li img', function() {
+	console.log("You are removing this item")
+	$(this).parent().remove();
+});
+
+});
